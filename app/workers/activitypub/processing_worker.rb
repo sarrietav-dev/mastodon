f8d2fd48f737cf74f3c0250d1dcbb1a0392c4bed
Ash Furrow <ash@ashfurrow.com>
2021-08-06 13:41:07 -0300

    Revert "Skips retries on IPv6-only instance."
    This reverts commit fdb4e0ed947a590521965caf76b73aac9fa0df8b.

--- app/workers/activitypub/processing_worker.rb
+++ app/workers/activitypub/processing_worker.rb
@@ -9,17 +9,5 @@ class ActivityPub::ProcessingWorker
     ActivityPub::ProcessCollectionService.new.call(body, Account.find(account_id), override_timestamps: true, delivered_to_account_id: delivered_to_account_id, delivery: true)
   rescue ActiveRecord::RecordInvalid => e
     Rails.logger.debug "Error processing incoming ActivityPub object: #{e}"
-  rescue HTTP::ConnectionError => e
-    # We're having a problem with federating with an IPv6-only instance. To
-    # avoid Sidekiq queues getting overwhelmed with retries, we're trying to
-    # skip retries on that specific instance.
-    # Longterm, we need to move away from Docker so that Mastodon has outbound
-    # IPv6 support. For now, this is the best we can do. The only other 
-    # workaround would be to block the instance, which is less fair.
-    if e.message.include?('failed to connect: No address for peertube.iselfhost.com')
-      Rails.logger.warn "Skipping a retry for ActivityPub object: #{e}, error: #{e}, #{e.message}"
-    else
-      raise # Re-raise error to retry Sidekiq job.
-    end
   end
 end
